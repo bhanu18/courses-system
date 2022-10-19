3a5511ff6d9353c3f844e2b1ad9645729e09a54e
bhanuvidh <bhanuvidh@windowslive.com>
2022-10-20 01:41:22 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://stirring-begonia-b90347.netlify.app",
   credentials: true
 }));
 
